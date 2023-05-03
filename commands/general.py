8783e3289dc048e2a1e299311743f1de189899e7
Kyle Minter <kyle.minter.1@gmail.com>
2023-05-03 18:54:33 -0500

    Rename 8ball command callback function, so cringe command functions properly.

--- commands/general.py
+++ commands/general.py
@@ -129,7 +129,7 @@ class GeneralExtension(Extension):
         description="Get a response from the 8ball",
         dm_permission=True
     )
-    async def cringe(self, context: InteractionContext):
+    async def eightball(self, context: InteractionContext):
         # List of potential 8ball responses. Source: https://magic-8ball.com/magic-8-ball-answers/
         responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good",
                      "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",

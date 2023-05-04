5b25532fd277eded781967bf1758d7999e5678a4
Kyle Minter <kyle.minter.1@gmail.com>
2023-05-03 19:15:29 -0500

    Bot no longer sends command tracebacks when not in testing mode.

--- minibot.py
+++ minibot.py
@@ -14,12 +14,22 @@ Database.setup_tag_database()
 
 # Create a client instance for connecting to discord.
 if (config["testing_mode_enabled"]):
-    # If the testing mode is enabled in the config we will set the debug scope to the guild ID specified in the config and enable the deletion of unused application commands.
+    # If the testing mode is enabled in the config we will set the debug scope to the guild ID specified in the config
+    # and enable the deletion of unused application commands along with the sending of command tracebacks.
     print("Testing mode enabled.\nSlash commands will be automatically instantiated with guild ID scope specified in config.")
-    client = interactions.Client(token=config["token"], intents=Intents.DEFAULT, delete_unused_application_cmds=True, debug_scope=config["testing_guild_id"])
+    client = interactions.Client(
+        token=config["token"],
+        intents=Intents.DEFAULT,
+        delete_unused_application_cmds=True,
+        send_command_tracebacks=True,
+        debug_scope=config["testing_guild_id"])
 else:
     # If the testing mode is disabled in the config we will setup the client as normal.
-    client = interactions.Client(token=config["token"], delete_unused_application_cmds=False, intents=Intents.DEFAULT)
+    client = interactions.Client(
+        token=config["token"],
+        delete_unused_application_cmds=False,
+        send_command_tracebacks=False,
+        intents=Intents.DEFAULT)
 
 # Listen for ready event.
 @interactions.listen()

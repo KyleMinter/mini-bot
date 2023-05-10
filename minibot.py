6c106105548f73d07c40691a425ff308d7ee80af
Kyle Minter <kyle.minter.1@gmail.com>
2023-05-10 16:10:02 -0500

    Adjusted imports.

--- minibot.py
+++ minibot.py
@@ -2,8 +2,7 @@ from util.config_manager import Config
 from util.database_manager import Database
 from extensions.database_cleanup import DatabaseCleanupExtension
 
-import interactions
-from interactions import Intents
+from interactions import Intents, Client, listen
 
 
 # Get the config for the bot.
@@ -17,7 +16,7 @@ Database.setup_bot_database()
 if (config["testing_mode_enabled"]):
     # If the testing mode is enabled in the config we will set the debug scope to the guild ID specified in the config and enable the sending of command tracebacks.
     print("Testing mode enabled.\nSlash commands will be automatically instantiated with guild ID scope specified in config.")
-    client = interactions.Client(
+    client = Client(
         token=config["token"],
         intents=Intents.new(default=True, message_content=True, guild_members=True),
         delete_unused_application_cmds=True,
@@ -26,14 +25,14 @@ if (config["testing_mode_enabled"]):
     print("")
 else:
     # If the testing mode is disabled in the config we will setup the client as normal.
-    client = interactions.Client(
+    client = Client(
         token=config["token"],
         intents=Intents.new(default=True, message_content=True, guild_members=True),
         delete_unused_application_cmds=True,
         send_command_tracebacks=False)
 
 # Listen for ready event.
-@interactions.listen()
+@listen()
 async def on_ready():
     DatabaseCleanupExtension.on_ready_cleanup(client)
     print(f"Logged in as {client.user}")
